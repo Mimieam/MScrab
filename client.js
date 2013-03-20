9f2e14758098e196aa82637ab982fa8d796bac4d
A. Miezan Echimane <t.echismimi@gmail.com>
2013-03-19 23:50:35 -0500

    validation working

--- client.js
+++ client.js
@@ -1,72 +1,5 @@
 $(document).ready(function() {
-	// Handler for .ready() called.
-$(function() { // some randome ruler ....
 
-	 var option = ' <li class = "config">' +
-				 '<select id="spacing">' +
-					'<option>1cm</option>' +
-					'<option>2cm</option>' +
-					'<option>1in</option>' +
-					'<option>1em</option>' +
-					'<option>70px</option>' +
-			 ' </select>' +
-	 ' </li>' ;
-
-
-		// Build "dynamic" rulers by adding items
-		$(".ruler[data-items]").each(function() {
-				var ruler = $(this).empty(),
-						len = Number(ruler.attr("data-items")) || 0,
-						item = $(document.createElement("li")),
-						i;
-				for (i = 0; i < len; i++) {
-						ruler.append(item.clone().text(i +1));
-				}
-			//ruler.append(option);
-		});
-		// Change the spacing programatically
-		function changeRulerSpacing(spacing) {
-				$(".ruler").
-					css("padding-right", spacing).
-					find("li").
-						css("padding-left", spacing);
-			$("#config").hide();
-		}
-		$("#spacing").change(function() {
-				//changeRulerSpacing($(this).val());
-				$(".ruler").attr("data-items","50");
-			$(".ruler[data-items]").empty();
-			$(".ruler[data-items]").each(function() {
-				var ruler = $(this).empty(),
-						len = Number(ruler.attr("data-items")) || 0,
-						item = $(document.createElement("li")),
-						i;
-				for (i = 0; i < len; i++) {
-						ruler.append(item.clone().text(i +1));
-				}
-			//ruler.append(option);
-		});
-
-			 changeRulerSpacing($(this).val());
-		});
-
-	/* hover the rulers and diplay the metric options  (M.O.)
-		 on exit of the M.O, hide it
-	*/
-	$("#config").hide();
-	$(".ruler").hover(function () {
-			$("#config").show();
-	}, function () {
-			$("#config").hover(function () {
-					$("#config").show();
-			}, function () {
-					$("#config").hide();
-			});
-
-	});
-
-
-});
 var CSSs = new (function  () {
 	this.spacing = 5;
 	this.cell_w = 70;
@@ -84,6 +17,7 @@ var CSSs = new (function  () {
 
 }) ();
 
+/*================================================================================================*/
 // only one copy of this object - add function here to describe other object
 Game = {};
 	Game.droppedInId;
@@ -92,6 +26,7 @@ Game = {};
 	Game.tileCnt = 0;
 	Game.chipCnt = 0;
 
+/*display*/
 Game.show_props = function (obj, objName) {
 	var result = "";
 		for (var prop in obj) {
@@ -101,20 +36,28 @@ Game.show_props = function (obj, objName) {
 };
 
 Game.validateBoard = function(client){
+		Mimi.equation = "";
+		Mimi.equationChip = [];
 		Mimi.validate();
  };
+
 Game.getRandomInt = function (min, max) {
-		return Math.floor(Math.random() * (max - min + 1)) + min;
+	return Math.floor(Math.random() * (max - min + 1)) + min;
 };
 Game.getRandomSign = function() {
-			var Signs = "+/*-";
-			var aSign;
-
-					aSign = Signs.charAt(Math.floor(Math.random() * Signs.length));
-			console.log(aSign);
-			return aSign;
+	var Signs = "+/*-";
+	var aSign;
+	aSign = Signs.charAt(Math.floor(Math.random() * Signs.length));
+	return aSign;
 };
+Game.getParentheses = function (orientation) {
+	if (orientation == "left")
+		return "(";
+	if (orientation == "right")
+		return ")";
+}
 
+/*================================================================================================*/
 function Chip(type,value) {
 	'use strict';
 	Game.chipCnt++;
@@ -164,6 +107,8 @@ function Chip(type,value) {
 
 
 }
+
+/*================================================================================================*/
 function Tile(T, L, status, type) {
 		'use strict';
 		Game.tileCnt++;
@@ -268,6 +213,8 @@ function Tile(T, L, status, type) {
 
 
 }
+
+/*================================================================================================*/
 function Region(name, startPt, endPt, Rule){
 	this.name = name;
 	this.startPt = startPt;
@@ -276,6 +223,7 @@ function Region(name, startPt, endPt, Rule){
 
 }
 
+/*================================================================================================*/
 function Board (divName,w ,h) {
 			this.width = w || $(divName).width();
 			this.height = h || $(divName).height();
@@ -304,11 +252,20 @@ Board.prototype = {
 	}
 
 };
-
-function ChipHolder() {
+/*================================================================================================*/
+function ChipHolder(_DOM_element) {
 	var number = [];  // public on purpose here... in production will be private
 	var opSign = [];
 	var eqSign = [];
+	var Symbols;
+	this.DOM_element = _DOM_element;
+	$(this.DOM_element).css("background-color","red").droppable({accept: 'div.chip' ,   hoverClass: 'hovered',    // need to be somewhere else...
+            drop: function (event, ui) {
+							ui.draggable.css({});
+							$(this).css({opacity: 0.8});
+							event.preventDefault();
+						}
+				});
 
 	function NewNumChip (numChip) {
 		number.push(numChip);
@@ -327,12 +284,14 @@ function ChipHolder() {
 			return eqSign.length;
 	}
 
+/* prepare the new chips*/
 	this.makeNewSet = function(_num,_op,_Eq) {
-		for (var i = _num; i > 0; i--) {
-			NewNumChip( new Chip("num", Game.getRandomInt(-9,9)));
+		for (var i = number.length|| _num; i > 0; i--) {
+			//NewNumChip( new Chip("num", Game.getRandomInt(-9,9)));  //-- give negative numbers
+			NewNumChip( new Chip("num", Game.getRandomInt(0,9)));
 		}
 
-		for (var j = _op; j > 0; j--) {
+		for (var j = opSign.length || _op; j > 0; j--) {
 			NewOpChip(new Chip("Op", Game.getRandomSign()));
 		}
 
@@ -341,66 +300,83 @@ function ChipHolder() {
 		}
 	};
 
-	this.getChips = function( divName) {
+/* push the new chips onto the rack*/
+	this.getChips = function( ) {
 
 		for (var i = number.length - 1; i >= 0; i--) {
-					$(divName).append(number[i].GetSelf()[0]);
+			$(this.DOM_element).append(number[i].GetSelf()[0]);
 		}
 
 		for (var x in opSign) {
-			$(divName).append(opSign[x].GetSelf()[0]);
+			$(this.DOM_element).append(opSign[x].GetSelf()[0]);
 		}
 
 		for (var y in eqSign) {
-			$(divName).append(eqSign[y].GetSelf()[0]);
+			$(this.DOM_element).append(eqSign[y].GetSelf()[0]);
 		}
 	};
 
-	this.getLength = function() {
-		return NewEqChip();
-	};
+	//for all new symbols to be added. this function will return a new chip with the appropriate symbol
+	this.getSymbols = function(symbol) {
+	if (symbol == "left-parenthesis"){
+		// for (var k = _Eq; k > 0; k--) {
+		// 	NewEqChip(new Chip("Eq", "="));
+		// }
+		return ((new Chip("left-parenthesis","(")).GetSelf()[0]);
+	}
+	if (symbol == "right-parenthesis" )
+		return ((new Chip("right-parenthesis",")")).GetSelf()[0]);
+	}
+
+	this.reset = function () {
+		number = [];
+		opSign = [];
+		eqSign = [];
+		Symbols = null;
+		$(this.DOM_element).empty();
+	}
+
 }
 
 
+/*================================================================================================*/
 
 var Client = function (name,homeTile) {
-		'use strict';
-		this.name=name;
-		var score = 0;
-		this.home = homeTile;
-		this.Move="";
-		this.equation ="";
-		this.usedChip = [];
-		this.usedTile = [];
-		this.rack = new ChipHolder();
-		this.rack.makeNewSet(5,2,1);  //5 numbers, 2 ops, 1 equal sign
-		$('#rack').css("background-color","red").droppable({accept: 'div.chip' ,   hoverClass: 'hovered',    // need to be somewhere else...
-            drop: function (event, ui) {
-							ui.draggable.css({});
-							$(this).css({opacity: 0.8});
-							event.preventDefault();
-						}
-				});
-	this.rack.getChips('#rack');
+	'use strict';
+	this.name=name;
+	var score = 0;
+	this.home = homeTile;
+	this.Move="";
+	this.equation ="";
+	this.equationChip = [];
+	this.usedChip = [];
+	this.usedTile = [];
+	this.rack = new ChipHolder('#rack');
+	this.rack.makeNewSet(5,2,1);  //5 numbers, 2 ops, 1 equal sign
+	//$(this.rack.DOM_element)
 
-		function pGetScore () { //private (pName) functions - not accessible anywhere but in here
-			return score;
-		}
+	this.rack.getChips();
+	$("#rack").append(this.rack.getSymbols("left-parenthesis"));
+	$("#rack").append(this.rack.getSymbols("right-parenthesis"));
 
-		function pSetScore(num) {
-			score +=num;
-		}
+	function pGetScore () { //private (pName) functions - not accessible anywhere but in here
+		return score;
+	}
 
-		this.getScore = function() {  // priviledge function - will access my private function - don't work with prototype... sigh..
-			return "you've got "+ pGetScore() + " points" ;
-		};
+	function pSetScore(num) {
+		score +=num;
+	}
 
-		this.setScore = function(num) {
-			pSetScore(num);
-		};
+	this.getScore = function() {  // priviledge function - will access my private function - don't work with prototype... sigh..
+		return "you've got "+ pGetScore() + " points" ;
+	};
 
-		this.findHome = function() {
-	return this.home;
+	this.setScore = function(num) {
+		pSetScore(num);
+	};
+
+	this.findHome = function() {
+		return this.home;
 	};
 
 	this.printUsedTile = function (argument) {
@@ -414,7 +390,7 @@ var Client = function (name,homeTile) {
 	function getChipNeighbor (orientation,element){
 		var Row = parseInt($(element).attr('data-row'), 10);    // (10) here is the radix parameter for parseInt
 		var Col = parseInt($(element).attr('data-col'), 10);
-		//console.log ("R "+ Row + " C "+Col);
+		// console.log ("orientation "+orientation);
 			switch(orientation){  // for future reference - this ugly selector get the chip at the specify posotion given by Row and Col
 				case 'left':
 							return  $('.chip[data-row='+ Row +'][data-col='+ (Col - 1) +']');
@@ -427,60 +403,78 @@ var Client = function (name,homeTile) {
 			}
 	}
 
-  // inspired by http://stackoverflow.com/a/4580171/623546
-  this.degradingRadiant = function  (argument,_time) {
-    console.log(argument);
-      var red1 = parseInt(argument,16) >> 16;
-      var green1 = (parseInt(argument,16) >> 8) & 0xFF;
-      var blue1  =  parseInt(argument,16) & 0xFF;
+  	// inspired by http://stackoverflow.com/a/4580171/623546
+    this.degradingRadiant = function  (argument,_time) {
+    	var red1 = parseInt(argument,16) >> 16;
+    	var green1 = (parseInt(argument,16) >> 8) & 0xFF;
+    	var blue1  =  parseInt(argument,16) & 0xFF;
 
-      var time = 0.3; // This should be between 0 and 1
+	    var time = _time; // This should be between 0 and 1
+	    var red2 = 0xFFFFFF >> 16;
+	    var green2 = (0xFFFFFF >> 8) & 0xFF;
+	    var blue2  = 0xFFFFFF & 0xFF;
 
-      var outred =  red1 * (time) ;
-      var outgreen =  green1 * (time) ;
-      var outblue =  blue1 *(time) ;
-      // var red2 = 0xFFFFFF >> 16;
-      // var green2 = (0xFFFFFF >> 8) & 0xFF;
-      // var blue2  = 0xFFFFFF & 0xFF;
+	    var outred = time * red1 + (1-time) * red2;
+	    var outgreen = time * green1 + (1-time) * green2;
+	    var outblue = time * blue1 + (1-time) * blue2;
+	    var hexColor = "#"+((1 << 24) + (outred << 16) + (outgreen << 8) + outblue).toString(16).substr(1);
+		return  hexColor ;
 
-      // var outred = time * red1 + (1-time) * red2;
-      // var outgreen = time * green1 + (1-time) * green2;
-      // var outblue = time * blue1 + (1-time) * blue2;
-       var hexColor = ((1 << 24) + (outred << 16) + (outgreen << 8) + outblue).toString(16).substr(1);
-return  hexColor ;
-  };
-this.lpcnt = 0;
-this.radCol = '7B3F00';
+	};
+
+	this.lpcnt = 0;
 	this.traverseUsedChip = function (direction,orientation,chip) {
 		var neighbor = chip;
-     this.lpcnt += 0.3;
+    	this.lpcnt += 0.3;
 		//console.log(neighbor);
 		if (neighbor[0] === undefined)
 			return 0 ;
 		else
 			{var col ;
 				if (direction == "horizontal"){
-					console.log(this.equation += neighbor.html());
-          console.log( col = this.degradingRadiant(this.radCol ,this.lpcnt));
-          this.radCol = col;
-          neighbor.css({"background-color":"#"+this.radCol});
-					this.traverseUsedChip (direction, orientation,getChipNeighbor(orientation, chip));
+		          console.log( col = this.degradingRadiant("7B3F00 ",this.lpcnt));
+		          neighbor.css({"background-color":col});
+		          console.log("orientation " + orientation);
+          			if (orientation == "left"){
+
+						this.traverseUsedChip (direction, orientation,getChipNeighbor(orientation, chip));
+						console.log(this.equation += neighbor.html()+"|");
+						this.equationChip.push(neighbor);
+
+          			}
+          			if (orientation == "right"){
+
+						this.equationChip.push(neighbor);
+						console.log(this.equation += neighbor.html()+"|");
+          				this.traverseUsedChip (direction, orientation,getChipNeighbor(orientation, chip));
+          			}
 				}
 				else{
 					if (direction == "vertical") {
-						console.log(this.equation += neighbor.html());
-						this.traverseUsedChip (direction, orientation,getChipNeighbor(orientation, chip));
+						if (orientation == "top"){
+
+							this.traverseUsedChip (direction, orientation,getChipNeighbor(orientation, chip));
+							this.equationChip.push(neighbor);
+							console.log(this.equation += neighbor.html()+"|");
+
+	          			}
+	          			if (orientation == "bottom"){
+	          				// console.log(" else orientation is right: -?" + orientation);
+							this.equationChip.push(neighbor);
+							console.log(this.equation += neighbor.html()+"|");
+	          				this.traverseUsedChip (direction, orientation,getChipNeighbor(orientation, chip));
+	          			}
 					}
 				}
 
 			}
 
-	};
+		};
 
-/* we are going from the last chip*/
+	/* we are going from the last chip*/
 	this.validate = function() {
 
-		console.log(this.printUsedTile());
+		// console.log(this.printUsedTile());
 		if (this.usedChip.length === 0)
 			return -1;
 		else {
@@ -490,25 +484,137 @@ this.radCol = '7B3F00';
 			var TN = getChipNeighbor('top',(this.usedChip[this.usedChip.length -1 ]))	||null;
 			var BN = getChipNeighbor('bottom',(this.usedChip[this.usedChip.length -1 ]))||null;
 
+			console.log("last used CHip :"+this.usedChip[this.usedChip.length -1 ].html());
 			if ((LN.length === 0 ? (RN.length === 0 ? false : true) : true )){
 				direction = "horizontal";
 				this.traverseUsedChip (direction,"left",LN);
-				console.log(this.equation += $(this.usedChip[this.usedChip.length -1 ]).html());
+				
+				//collect the chip for validation
+				this.equationChip.push(this.usedChip[this.usedChip.length -1 ]);
+				console.log(this.equation += $(this.usedChip[this.usedChip.length -1 ]).html()+"|");	
+
 				this.traverseUsedChip (direction,"right",RN);
 			}
 			else {
 					if ((TN.length === 0 ? (BN.length === 0 ? false : true) : true )){
-					direction = "horizontal";
-					this.traverseUsedChip (direction,"left",LN);
-					console.log(this.equation += $(this.usedChip[this.usedChip.length -1 ]).html());
-					this.traverseUsedChip (direction,"right",RN);
+					direction = "vertical";
+					this.traverseUsedChip (direction,"top",TN);
+
+					this.equationChip.push(this.usedChip[this.usedChip.length -1 ]);
+					console.log(this.equation += $(this.usedChip[this.usedChip.length -1 ]).html()+"|");
+					
+					this.traverseUsedChip (direction,"bottom",BN);
 				}
 			}
 
 		}
+		// console.log(999+  parseInt("-1000", 10));
+		console.log("let's parse the equation: ");
+		//console.log(this.equationChip);
+		var equation = this.equation.replace(/\|/g,'')
+		console.log(equation);
+		var result = this.parseEquation(equation);
+		console.log(result);
+		if (1){
+			this.rack.reset();
+			this.rack.makeNewSet(5,2,1);
+			this.rack.getChips();
+		}
+		this.equation = "";
+		this.equationChip = [];
 	};
 
+	this.parseEquation = function (_myEquation) {
+		return module.exports.parse(_myEquation);
+	// 	var res = 0,
+	// 		aChar,
+	// 		current_Char,
+	// 		maching_parenthesis = 0,
+	// 		previous_Char,
+	// 		next_Char,
+	// 		myEquation = _myEquation.split("|");
+	// 		console.log("Current Ebquation: "+ myEquation);
+	// 	for (var i = 0 ; i <= myEquation.length - 1; i++) {
+
+	// 			if (i > 0) previous_Char = myEquation[i-1];
+	// 			current_Char = myEquation[i];
+	// 			next_Char = myEquation[i+1];
+	// 			if((next_Char !== undefined) && (next_Char !== "")){
+
+	// 				console.log("current - "+current_Char + " next - " + next_Char );
+
+	// 				switch(current_Char){
+	// 					case '+' :
+	// 						if (typeof(next_Char) !== Number || next_Char != '('){
+	// 							console.log("error on addition case");
+	// 						}
+	// 						break;
+
+	// 					case '-' :
+	// 						if (typeof(next_Char) !== Number || next_Char != '('){
+	// 							console.log("error on substraction case");
+	// 						}
+	// 						break;
+
+	// 					case '*' :
+	// 						if (typeof(next_Char) !== Number || next_Char != '('){
+	// 								console.log("error on substraction case");
+	// 							}
+	// 						break;
+
+	// 					case '/' :
+	// 						if (typeof(next_Char) !== Number || next_Char != '('|| next_Char == 0){
+	// 								console.log("error on substraction case");
+	// 							}
+	// 						break;
+
+	// 					case '(' :
+	// 						maching_parenthesis +=1;
+	// 						if (typeof(next_Char) !== Number || next_Char != '('|| next_Char == 0){
+	// 								console.log("error on substraction case");
+	// 							}
+	// 						break;
+
+	// 					case ')' :
+	// 						maching_parenthesis -=1;
+	// 						break;
+
+	// 					case '=' :
+	// 						break;
+	// 					default :
+	// 					//should be a number - might be a negative number
+	// 						try {
+	// 							chr = parseInt(current_Char,10);
+	// 						}
+	// 						catch(e){
+	// 							console.log("unknown character ")
+	// 						}
+	// 				}
+
+	// 				// if (aChar == NaN ){
+	// 				// 	if (myEquation[i++] !== undefined){
+	// 				// 		i++;
+	// 				// 		res = res / myEquation[i];
+	// 				// 	}else {
+	// 				// 		console.log("error : Invalid equation in this.parseEquation()");
+	// 				// 		return  -1;
+	// 				// 	}
+	// 				// }
+	// 				res = res + parseInt(myEquation[i], 10);
+	// 				// console.log(typeof(res)+" res: "+ res);
+	// 				}
+	// 	if (maching_parenthesis != 0){console.log('error - not matching parenthesis');}
+	// 	}; 
+	// 	// for (var i = 0; i < s.length; i++) {
+	// 	//     console.log(s.charAt(i));
+	// 	// }
+	// 	// console.log("final res:"+ res);
+	 }
+
+	// // this.
+
 };
+/*================================================================================================*/
 
 
 test("ChipHolder Test", function () {
