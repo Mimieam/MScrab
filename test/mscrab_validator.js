547749e2b69caf3a6284be8220a7776f78b2ab67
Mimieam <t.echismimi@gmail.com>
2014-12-21 05:29:03 -0600

    the extended equations are better now

--- test/mscrab_validator.js
+++ test/mscrab_validator.js
@@ -6,6 +6,21 @@
 // [{x:2,y:3},{x:4,y:5},{x:6,y:7},{x:5,y:5}].sort(function(a,b){return (a.x >= b.x)?1:-1 });
 
 
+var board = makeFakeBoard(9, 20);
+    getFakeBoard(board);
+    // layItAllDown(direction,row,col,"a=b+c",board)
+    layItAllDown('vertical',2,3,"2=8-6",board)
+    layItAllDown('horizontal',3,0,"2=1+1",board)
+    layItAllDown('horizontal',5,7,"2=2",board)
+    layItAllDown('vertical',1,0,"2=1+1",board)
+    layItAllDown('horizontal',4,0,"3=3=3",board)
+var StartH = {x:3,y:4},
+    EndH = {x:4,y:4},
+    StartV = {x:4,y:3},
+    EndV = {x:4,y:5};
+// _getExtendedStartAndEndTiles({y:3,x:0},{y:3,x:1});
+// _getExtendedStartAndEndTiles({y:3,x:0},{y:4,x:1},"horizontal");
+
 // the 2 following functions _checkVertical && _checkHorizontal are to be used with the function Array.every();
 
 // check if the tiles are aligned verically - that is if all x's are the same
@@ -28,7 +43,7 @@ function _checkHorizontal(element, index, array){
   Input: Set array of tiles
   Output: the direction of the equation , horizontal , vertical or null
 */
-function getOrientation (Set) {
+function _getOrientation (Set) {
   if (Set.length == 0)
     return null;
   if( Set.every(_checkHorizontal) == true )
@@ -45,12 +60,16 @@ function getOrientation (Set) {
   Output: The first and last tile and the axis of comparison
 */
 function _getStartAndEndTiles(Set , orientation){
-  if(orientation!="vertical"&&orientation!="horizontal") // should never be the case... but just to be safe^^
+  if(orientation!="vertical"&&orientation!="horizontal"){ // should never be the case... but just to be safe^^
+    console.log("hellow ...");
     return null;
+  }
   var highest = Set[0];
   var lowest   = Set[0];
   var Axis = (orientation[0]=='v' ? 'y' :'x');   // ok i did get lazy here... hey it's 1:25 AM.. well this says: if direction is vertical we check the y's
     // the start of the equation is the lowest end...  most right or bottom ...  ( but in the actuall evaluation it doesn't matter because the parser doesn't care of the direction of the equation only he oriendation) 
+  
+  console.log(Axis, Set, highest[Axis])
   for(var i=1;i<Set.length;i++) {
     lowest   = (Math.min(lowest[Axis] , Set[i][Axis]) == lowest[Axis]  ? lowest  : Set[i] );     //Starting tile
     highest     = (Math.max(highest[Axis], Set[i][Axis]) == highest[Axis] ? highest : Set[i] );  //Ending tile
@@ -64,66 +83,80 @@ function _getStartAndEndTiles(Set , orientation){
    Start   - starting coordinate of an equation
    end     - ending coordinate of an equation
    Axis    - changing component of the coordinate
-   otherAxis - non changing component of the cordinate
+   otherAxis - non changing component of the coordinate
    orientation - horizontal or vertical
    ActualSet - set of coordinate between Start and End, included.
+   _toString - string of all the tiles involved in the equation
   }
 */
 
-function genEquation (Set) {
+function genEquation (Set, hardSetOrientation) {
     var equation = {},
-        orientation = getOrientation(Set);
-    
-    if (orientation == null)
+        orientation = typeof hardSetOrientation !== 'undefined' ? hardSetOrientation : _getOrientation(Set);
+
+    if (orientation == null){
       return null;
+    }
     
     // get Starting and ending tiles
-    var tmp = _getStartAndEndTiles(Set,orientation); 
-    equation = tmp;   // get Start , End & Axis from here
+    var InitialSet = _getStartAndEndTiles(Set, orientation); 
+    // console.log("InitialSet: ", InitialSet)    
+
+    if (InitialSet == null)
+      return null;
+    extendedSet = _getExtendedStartAndEndTiles(InitialSet.Start, InitialSet.End, orientation )
+    // console.log( "extended: --> ",extendedSet)
+    equation = extendedSet;   // get Start , End & Axis from hered
+    equation.InitialSet = InitialSet
     equation.ActualSet = [];
     equation.orientation = orientation;
     var otherAxis = (equation.Axis == 'x')?'y':'x';   // the one whose value do not change in the tiles.
 
     //get all tiles between Start and End  -  basically we collect the coordinate of each tile to be involve in the evaluation   // this way we can collect innactive tiles and also check them if there are used in the equation.
     var newTile = {};
+    equation._toString = ""
 
     for (var i = equation.Start[equation.Axis]; i <= equation.End[equation.Axis]; i++) {
       newTile = {};      //NUKE THE REFERENCE !!!! stupid shallow copy >_<  spent 30 min wondering why all the value where the same in equation.ActualSet --' --> see answer here : http://stackoverflow.com/questions/8660901/do-objects-pushed-into-an-array-in-javascript-deep-or-shallow-copy
       newTile[equation.Axis] = i;
       newTile[otherAxis] = equation.Start[otherAxis]; // Start[otherAxis]  == End[otherAxis] since those tiles are on the same axis.. or orientation
       equation.ActualSet.push(newTile); 
+      // console.log(newTile)
+      equation._toString += _getTileStateAndContent(newTile.x, newTile.y).content // DOM access 
     };
     equation.otherAxis = otherAxis;
     return equation;
 }
 
 // TEST _getStartAndEndTiles
-_getStartAndEndTiles([{x:2,y:5},{x:4,y:5},{x:6,y:5},{x:5,y:5}],"horizontal")  ; //  should return ["Start":{x:2,y:5},"End":{x:6,y:5}, Axis: x]
-_getStartAndEndTiles([{x:3,y:2},{x:3,y:2},{x:3,y:2},{x:3,y:5}],"vertical") ;  // should return ["Start":{x:3,y:2},"End":{x:3,y:5}, Axis: y]
-_getStartAndEndTiles([{x:3,y:2},{x:3,y:2},{x:3,y:2},{x:3,y:5}],"alksdfjlk") ;  // should return null
+// console.log(_getStartAndEndTiles([{x:3,y:2},{x:3,y:2},{x:3,y:2},{x:3,y:5}],"vertical")) ;  // should return ["Start":{x:3,y:2},"End":{x:3,y:5}, Axis: y]
+// console.log(_getStartAndEndTiles([{x:2,y:5},{x:4,y:5},{x:6,y:5},{x:5,y:5}],"horizontal") ) ; //  should return ["Start":{x:2,y:5},"End":{x:6,y:5}, Axis: x]
+// console.log(_getStartAndEndTiles([{x:3,y:2},{x:3,y:2},{x:3,y:2},{x:3,y:5}],"alksdfjlk")) ;  // should return null
 
-//Open your console for dirty testing :)
+// //Open your console for dirty testing :)
 
-//TEST _checkVertical
-[{x:3,y:2},{x:3,y:2},{x:3,y:2},{x:3,y:5}].every(_checkVertical) ; // should be true
-[{x:3,y:2},{x:3,y:2},{x:4,y:2},{x:5,y:5}].every(_checkVertical) ; // should be false
+// //TEST _checkVertical
+// [{x:3,y:2},{x:3,y:2},{x:3,y:2},{x:3,y:5}].every(_checkVertical) ; // should be true
+// [{x:3,y:2},{x:3,y:2},{x:4,y:2},{x:5,y:5}].every(_checkVertical) ; // should be false
 
-//TEST _checkHorizontal
-[{x:2,y:5},{x:4,y:5},{x:6,y:5},{x:5,y:5}].every(_checkHorizontal) ;   // should be true
-[{x:2,y:5},{x:4,y:5},{x:6,y:5},{x:5,y:2}].every(_checkHorizontal);   // should be false
+// //TEST _checkHorizontal
+// [{x:2,y:5},{x:4,y:5},{x:6,y:5},{x:5,y:5}].every(_checkHorizontal) ;   // should be true
+// [{x:2,y:5},{x:4,y:5},{x:6,y:5},{x:5,y:2}].every(_checkHorizontal);   // should be false
 
-// TEST getOrienation
-getOrientation([{x:3,y:5},{x:3,y:5},{x:3,y:5},{x:4,y:4}]) ;  // should be null
-getOrientation([{x:2,y:5},{x:4,y:5},{x:6,y:5},{x:5,y:5}]) ;  // should be Horizontal
-getOrientation([{x:3,y:2},{x:3,y:2},{x:3,y:2},{x:3,y:5}]) ;  // should be vertical
-getOrientation([{x:3,y:3},{x:3,y:3},{x:3,y:3},{x:3,y:3}]) ;  // should be horizontal -- > because it's priority( checked first) is given to horizontal equations.. for obvious reasons..  
+// // TEST getOrienation
+// _getOrientation([{x:3,y:5},{x:3,y:5},{x:3,y:5},{x:4,y:4}]) ;  // should be null
+// _getOrientation([{x:2,y:5},{x:4,y:5},{x:6,y:5},{x:5,y:5}]) ;  // should be Horizontal
+// _getOrientation([{x:3,y:2},{x:3,y:2},{x:3,y:2},{x:3,y:5}]) ;  // should be vertical
+// _getOrientation([{x:3,y:3},{x:3,y:3},{x:3,y:3},{x:3,y:3}]) ;  // should be horizontal -- > because it's priority( checked first) is given to horizontal equations.. for obvious reasons..  
 
 // TEST genEquation
-genEquation([{x:1,y:3},{x:3,y:5},{x:2,y:4}]);;  // should be null
-genEquation([{x:1,y:3}])  ;// should be {x:1,y:3}
-genEquation([{x:2,y:3},{x:2,y:5}]) ; // should be {x:2,y:3},{x:2,y:4},{x:2,y:5}
+// genEquation([{x:1,y:3},{x:3,y:5},{x:2,y:4}]);;  // should be null
+// genEquation([{x:1,y:3}])  ;// should be {x:1,y:3}
+// myEQ = genEquation([{x:0,y:4},{x:9,y:4}]) ; // should be {x:2,y:3},{x:2,y:4},{x:2,y:5}
+// // console.log("myEQ -->\n" , myEQ)
 
 
+console.log(validate([{x:0,y:4},{x:3,y:4}]))
 /*  !!! - This function efficiency can be altered depending on how the DOM elements are accessed...
   This function will expand an equation to capture all
   used and disabled tile that are connected to the new equation 
@@ -132,37 +165,55 @@ genEquation([{x:2,y:3},{x:2,y:5}]) ; // should be {x:2,y:3},{x:2,y:4},{x:2,y:5}
   is met ( and we don't take the equal sign) or there is no more connected tiles,
   same for the End coordinates.
   Input: Start, End  - obtain from genEquation
-  output: newStart, newEnd - expended equation will all tiles connected to it.
+  output: newStart, newEnd, axis - expended equation will all tiles connected to it.
   Testing: make use of _getFakeTileState() and global fake Board ^^ 
 */
-function _getExtendedStartAndEndTiles(Start, End) {
-  if((orientation = getOrientation([Start,End])) == null){
+
+function extendEquation(equation){
+  eq = _getExtendedStartAndEndTiles(equation.Start, equation.End)
+
+
+}
+
+function _getExtendedStartAndEndTiles(Start, End, hardSetOrientation) {
+  if((orientation = _getOrientation([Start,End])) == null){
     console.log("Wrong orientation... no diagonal equation allowed as of now... sorry")
     return null
   }
+  orientation = typeof hardSetOrientation !== 'undefined'? hardSetOrientation:  _getOrientation([Start,End]);
+  console.log("orientation from extended ==>", orientation)
+  // shortcut for bypassing extending equation with 
   
   var newStart = Start,
-    newEnd = End,
+    newEnd = {},
     totalEquals = 0;
     newStart.type = "Start";
     newEnd.type = "End";
+    newEnd.x= End.x;
+    newEnd.y= End.y;
+    if (Start == End){
+      console.log("STARTING TILE == ENDING TILE");
+    }
 
-  //get find new Start / End coordinate
+    console.log("newStart", newStart);
+    console.log("newEnd", newEnd);
+
+  //find new Start / End coordinate
 
   [newStart, newEnd].forEach(function(T){ 
     var inc = T.type == "Start"? -1 : 1;   // if Start then get element before, else get element after tile T
-    console.log(T.type + " began");
+    // console.log(T.type + " began");
     var tile = {
       x: (orientation == "horizontal") ? T.x + inc : T.x,
       y: (orientation == "vertical")   ? T.y + inc : T.y
     };
-    // var res = _getTileState(tile.x,tile.y);   // DOM access to check value of tile.
+    // var res = _getTileStateAndContent(tile.x,tile.y);   // DOM access to check value of tile.
     var res = _getFakeTileState(tile.x,tile.y,board);   // DOM access to check value of tile.
     
     var tmp = tile;    
-    while(res.state == "disabled" && (res.content != '=' || totalEquals != 2)) {
-      console.log(res.content+res.state+orientation);
-        console.log(tile);
+    while(res.state == "disabled" && (res.content != '=' || res.content !='.' || totalEquals != 2)) {
+      // console.log(res.content+res.state+orientation);
+      //   console.log(tile);
       (T.type == "Start") ? newStart = tile : newEnd = tile;
       tmp = tile;
       tile = {};
@@ -174,23 +225,47 @@ function _getExtendedStartAndEndTiles(Start, End) {
         y: (orientation == "vertical")   ? tmp.y + inc : tmp.y
       };
       if (res.content == '=') totalEquals++;
-      // res = _getTileState(tile.x,tile.y); 
+      // res = _getTileStateAndContent(tile.x,tile.y); 
       res = _getFakeTileState(tile.x,tile.y,board); 
     }
-    console.log(T.type + " ended");
+    // console.log(T.type + " ended");
   });
   var Axis = (orientation[0]=='v' ? 'y' :'x');
+
   return{"Start":newStart,"End":newEnd,"Axis":Axis};
 }
 /*
   return state of a tile on a board.
 */
-function _getTileState(x,y) {
+function _getTileStateAndContent(x,y) {
   //TODO: find tile in DOM with corresponding x and y
   // var board =  getFakeBoard()
   // for now just randomly return a state  
-  var res = {"state": (Math.floor(Math.random()*2) == 0) ? null : "disabled" ,  "content":(Math.floor(Math.random()*6) == 5) ? 'x' : "="};
-  return res;
+  // var res = {"state": (Math.floor(Math.random()*2) == 0) ? null : "disabled" ,  "content":(Math.floor(Math.random()*6) == 5) ? 'x' : "="};
+  // return res;
+  return _getFakeTileState(x,y,board)
+}
+
+
+function evaluate_equation_string (equation) {
+  console.log("evaluating this equation\n", equation)
+  if (equation == null)
+    return false
+  components = equation._toString.split("=")
+  current_result = eval(components[0])
+  for (var i = components.length - 1; i >= 1; i--) {
+    if (eval(components[i]) != current_result)
+      return {"status":false, "score":0}
+  };
+  return {"status":true, "score":current_result*components.length}
+}
+
+function _validate_a_single_equation(equation){
+  return evaluate_equation_string(equation)
+}
+
+function _axis_to_orientation (axis){
+  return axis == "y" ? "vertical": "horizontal";  
 }
 
 function validate (Set) {
@@ -209,6 +284,26 @@ function validate (Set) {
 
     return _computePoints([set of all equations])
 */
+  isValid = true
+  primaryEq = genEquation(Set)
+  isValid = _validate_a_single_equation(primaryEq).status
+  for (var TileIndex = primaryEq.ActualSet.length - 1; TileIndex >= 0; TileIndex--) {
+    if (isValid == true){ 
+      // secondaryEq = _getExtendedStartAndEndTiles(primaryEq.ActualSet[TileIndex], primaryEq.ActualSet[TileIndex]) // use same time to generate secondaty equation
+      // console.log("ActualSet - >",primaryEq.ActualSet)
+      secondaryEq = genEquation([primaryEq.ActualSet[TileIndex]], _axis_to_orientation(primaryEq.otherAxis)) // use same time to generate secondaty equation
+      console.log("secondary equations",secondaryEq)
+      if (secondaryEq != null)
+        isValid = _validate_a_single_equation(secondaryEq).status
+        if (isValid != true){
+          return false
+        }
+    }
+  };
+
+  return isValid
+
+
 
 }
 
@@ -216,20 +311,20 @@ function validate (Set) {
 
 // ALL THe Following functions are FAKE and used to test _getExtendedStartAndEndTiles()
 
-var board = makeFakeBoard(9, 20);
-    getFakeBoard(board);
-    // layItAllDown(direction,row,col,"a=b+c",board)
-    layItAllDown('vertical',2,3,"2=8-6",board)
-    layItAllDown('horizontal',3,0,"2=1+1",board)
-    layItAllDown('horizontal',5,7,"2=2",board)
-    layItAllDown('vertical',1,0,"2=1+1",board)
-    layItAllDown('horizontal',4,0,"23=3-3+3=3",board)
-var StartH = {x:3,y:4},
-    EndH = {x:4,y:4},
-    StartV = {x:4,y:3},
-    EndV = {x:4,y:5};
-_getExtendedStartAndEndTiles({y:3,x:0},{y:3,x:1});
-_getExtendedStartAndEndTiles({y:3,x:0},{y:4,x:1},"horizontal");
+// var board = makeFakeBoard(9, 20);
+//     getFakeBoard(board);
+//     // layItAllDown(direction,row,col,"a=b+c",board)
+//     layItAllDown('vertical',2,3,"2=8-6",board)
+//     layItAllDown('horizontal',3,0,"2=1+1",board)
+//     layItAllDown('horizontal',5,7,"2=2",board)
+//     layItAllDown('vertical',1,0,"2=1+1",board)
+//     layItAllDown('horizontal',4,0,"23=3-3+3=3",board)
+// var StartH = {x:3,y:4},
+//     EndH = {x:4,y:4},
+//     StartV = {x:4,y:3},
+//     EndV = {x:4,y:5};
+// _getExtendedStartAndEndTiles({y:3,x:0},{y:3,x:1});
+// _getExtendedStartAndEndTiles({y:3,x:0},{y:4,x:1},"horizontal");
 
 function _getFakeTileState(x,y,board) {  
   //x = columns & y = row
@@ -300,13 +395,14 @@ function makeFakeBoard (MaxR,MaxC) {
 function getFakeBoard (board, noCoordinate) {
   // var board = makeFakeBoard(Max);
   var b = "";
+  console.log("{x,y}")
   board.every(function (row, index, array){
   // console.log(row) 
     row.every(function (obj, i, a){
       if(noCoordinate != undefined)
       b+= obj.content+' ';
       else         
-      b+='{'+obj.y+','+obj.x+'} '+obj.content+' ';
+      b+='{'+obj.x+','+obj.y+'} '+obj.content+' ';
       // console.log(obj);
       return true;
     });
